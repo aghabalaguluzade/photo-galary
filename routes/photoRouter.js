9980524995a23ab66cd959e4ad8cb848d945cecb
aghabalaguluzade <agabala.oyunda@gmail.com>
2022-09-19 01:48:48 +0400

    photo-galary

--- routes/photoRouter.js
+++ routes/photoRouter.js
@@ -9,5 +9,6 @@ router.route("/")
 
 router.route("/:id").get(photoController.getPhoto);
 router.route("/:id").delete(photoController.deletePhoto);
+router.route("/:id").put(photoController.updatePhoto);
 
 export default router;
