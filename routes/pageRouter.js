11e4adb5271612788c6e12ffe769f53ba03bdc58
aghabalaguluzade <agabala.oyunda@gmail.com>
2022-09-19 15:34:23 +0400

    photo-galary

--- routes/pageRouter.js
+++ routes/pageRouter.js
@@ -9,5 +9,7 @@ router.route("/about").get(pageController.getAboutPage);
 router.route("/register").get(pageController.userRegister);
 router.route("/login").get(pageController.userLogin);
 router.route("/logout").get(pageController.getLogout);
+router.route("/contact").get(pageController.getContactPage);
+router.route("/contact").post(pageController.sendMail);
 
 export default router;
